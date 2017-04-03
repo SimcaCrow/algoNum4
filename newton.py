8cbdd9a78d4a4df5e2ded9c1dd10d0251cf3691a
Camille El-Habr <cel-ha910e@enseirb-matmeca.fr>
2017-04-03 11:45:51 +0200

    Create newton.py

---
+++ newton.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+# ---------------------------------#
+"""
+File : test.py
+Author : Guichard A.,  Calandra J.
+Description : 4ième TD algorithme numerique
+"""
+# ---------------------------------#
+
+import numpy as np
+
+# ---------------------------------#
+
+
+# ---------------------------------#
+if __name__ == '__main__':
+
+    print("Test")
+
+# ---------------------------------#
