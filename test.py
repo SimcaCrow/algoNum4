6ececdde2f48ab7cfa896754a4af486c746cdd6e
El-Habr Camille <Simca@localhost.localdomain>
2017-04-03 11:38:58 +0200

    test.py vide, à compléter

---
+++ test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+# ---------------------------------#
+"""
+File : test.py
+Author : Calandra J., Guichard A.
+Description : 4ième TD algorithme numerique
+"""
+# ---------------------------------#
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ---------------------------------#
+# Test
+# ---------------------------------#
+
+
+# ---------------------------------#
+if __name__ == '__main__':
+
+    
+
+# ---------------------------------#
