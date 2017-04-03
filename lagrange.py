28e695f0e46a695c5c8e6d813da5748741286bc2
Camille El-Habr <cel-ha910e@enseirb-matmeca.fr>
2017-04-03 11:58:14 +0200

    Create lagrange.py

---
+++ lagrange.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+# ---------------------------------#
+"""
+File : test.py
+Author : El-Habr C.
+Description : 4ième TD algorithme numerique
+"""
+# ---------------------------------#
+
+import numpy as np
+
+# ---------------------------------#
+
+
+# ---------------------------------#
+if __name__ == '__main__':
+
+    print("Test")
+
+# ---------------------------------#
