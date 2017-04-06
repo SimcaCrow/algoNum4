87fad6fa954c75c965eb6b3b0b80578db39a3361
GUICHARD Alois <aguichard002@ssh.enseirb-matmeca.fr>
2017-04-06 15:34:07 +0200

    maj newton.py

--- newton.py
+++ newton.py
@@ -23,3 +23,19 @@ def Newton_Raphson(f, J, U0, N, epsilon):
         V = tmp[0]
         U = U+V
     assert(False and "precision non atteinte")
+
+def Newton_Raphson_back(f, J, x0, N, epsilon):
+    x = x0
+    for i in range(N):
+        print(i)
+        va = f(x)
+        na = np.linalg.norm(va)
+        if na < epsilon:
+            return x
+        dv = J(x)
+        dx = np.linalg.lstsq(dv,-va)[0]
+        st = 1.0
+        while np.linalg.norm(f(x+st*dx)) >= na+0.001:
+            st *= 2.0/3.0
+        x += st*dx
+    assert(False and "precision non atteinte")
--- test.py
+++ test.py
@@ -10,14 +10,51 @@ Description : 4ième TD algorithme numerique
 
 import numpy as np
 import matplotlib.pyplot as plt
+from newton import *
 
 # ---------------------------------#
 # Test
 # ---------------------------------#
+def Newton_Raphson_Test():
+    f = lambda x:np.sin(x)
+    J = lambda x:np.cos(x)
+    f3 = lambda x:np.array([x[0]*x[0]-2,x[1]*x[1]-x[1]-1])
+    J3 = lambda x:np.array([[2*x[0],0],[0,2*x[1]-1]])
+    
+    def f2(x):
+        res = np.mat(np.zeros((2,1)))
+        res[0,0]=x[0,0]**2+x[1,0]
+        res[1,0]=x[0,0]-x[1,0]**2
+        return res
+    
+    def J2(x):
+        res = np.mat(np.zeros((2,2)))
+        res[0,1]=1
+        res[0,0]=2*x[0,0]
+        res[1,0]=-2*x[1,0]
+        res[1,1]=1
+        return res
+    
+    x= np.mat(np.zeros((1,1)))
+    x[0,0] = 1
+    
+    x2=np.mat(np.zeros((2,1)))
+    x2[0,0]=9
+    x2[1,0]=7
+    
+    x3=np.array([[2],[2]])
+    """
+    print(Newton_Raphson(f,J,x,1000,10**-10))
+    
+    print(Newton_Raphson(f2,J2,x2,1000,10**-10))
 
+    print(Newton_Raphson(f3, J3,  x3, 1000, 10**-10))
+    print(Newton_Raphson_back(f3, J3,  x3, 1000, 10**-10))
+    """
 # ---------------------------------#
 if __name__ == '__main__':
 
-    print("Test de Newton_Raphson")
-
+    print("Test de Newton_Raphson...")
+    Newton_Raphson_Test()
+    print("Test OK")
 # ---------------------------------#
