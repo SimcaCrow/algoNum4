491deea0f9dbbd221452e2c091cc78d68c9c29b0
AlphaGolf33 <vous@exemple.com>
2017-04-10 12:12:01 +0200

    raclette

--- electrostatic_equilibrium.py
+++ electrostatic_equilibrium.py
@@ -8,11 +8,15 @@ Description : 4ième TD algorithme numerique
 """
 # ---------------------------------#
 
+from newton import *
 import numpy as np
 
 # ---------------------------------#
 
-def Grad_E(X):
+# Calcul du gradient de l'energie
+# X : un vecteur de positions
+# renvoie un vecteur
+def grad_E(X):
     n, m = np.shape(X)
     assert(m == 1)
     res = np.zeros((n,1))
@@ -21,5 +25,15 @@ def Grad_E(X):
         for j in range(n):
             if (i != j):
                 s += 1./(X[i,0] - X[j,0])
-        res[i, 0] = 1./(X[i,0]+1) + 1./(X[i,0]-1) + s
+        res[i, 0] = 1./X[i,0]+1 + 1./X[i,0]-1 + s
     return res
+
+# Calcul de la position d'équilibre pour
+# n charges
+# retourne le vecteur des positions
+def pos_equilibre(n):
+    U0 = np.zeros((n,0))
+    N = 1000
+    eps = 10**-5
+    U = Newton_Raphson_back(grad_E, J, U0, N, eps)
+    return U
